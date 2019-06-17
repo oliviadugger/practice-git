d664c3f9e1f8c69d50490ac574c8c0eb0eb0ab08
Olivia Dugger <oliviacdugger@gmail.com>
2019-06-17 14:29:29 -0500

    Initial Commit

---
+++ practice-git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice-git
+//
+//  Created by Olivia Dugger on 6/17/19.
+//  Copyright © 2019 Olivia Dugger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
